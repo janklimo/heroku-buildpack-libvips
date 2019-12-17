f8e8835b3e662888fde9a95ca90b4d53080e0cbb
Jan Klimo <jan.klimo@gmail.com>
2019-12-17 21:34:54 +0700

    Configure env variables

--- .profile.d/libvips.env.sh
+++ .profile.d/libvips.env.sh
@@ -1,1 +1,3 @@
 export PATH="$PATH:$HOME/.heroku/vips/bin"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/.heroku/vips/lib/pkgconfig"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.heroku/vips/lib"
